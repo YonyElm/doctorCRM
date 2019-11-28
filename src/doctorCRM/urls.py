25e0c8bfc2b1b229b07abcd26d13dfc9c66676e4
Techye <yony.elm@gmail.com>
2019-11-27 23:14:10 -0800

    Starting Django application

--- src/doctorCRM/urls.py
+++ src/doctorCRM/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('app.urls')), # No suffix to URL will lead to application
     path('admin/', admin.site.urls),
 ]
