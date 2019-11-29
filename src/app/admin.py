186dbe5fa0f87bcde006b6f1236b9f72f46ee10a
Techye <yony.elm@gmail.com>
2019-11-29 08:30:31 -0800

    Create SQL schema for Data base

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Patient
+from .models import Test
+from .models import TrackingChart
+from .models import Doctor
+
+admin.site.register(Patient)
+admin.site.register(Test)
+admin.site.register(Doctor)
+admin.site.register(TrackingChart)
