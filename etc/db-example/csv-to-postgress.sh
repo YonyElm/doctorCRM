8531e2771371ab49098e1bc7d8ddc7900d0edb7f
TechyE <Yony@TechyE.com>
2019-12-22 21:22:59 -0800

    Load and fetch data from DB to .csv files

---
+++ etc/db-example/csv-to-postgress.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for file in "$SCRIPT_DIR"/*.csv; do
+  sudo docker cp "$file" postgres_db:/tmp/;
+done;
+
+echo "The default password for 'user' is 123456789"
+psql -h localhost -p 5000 -U user -d doctorCRM -c "COPY app_doctor FROM '/tmp/app_doctor.csv' DELIMITER ',' CSV HEADER;"
+psql -h localhost -p 5000 -U user -d doctorCRM -c "COPY app_patient FROM '/tmp/app_patient.csv' DELIMITER ',' CSV HEADER;"
+psql -h localhost -p 5000 -U user -d doctorCRM -c "COPY app_test FROM '/tmp/app_test.csv' DELIMITER ',' CSV HEADER;"
+psql -h localhost -p 5000 -U user -d doctorCRM -c "COPY app_trackingchart FROM '/tmp/app_trackingchart.csv' DELIMITER ',' CSV HEADER;"
