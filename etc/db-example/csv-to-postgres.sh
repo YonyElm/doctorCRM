0dae3817beeb9c8a54942dea0a8270bb77505c10
techyE <34646433+techyE@users.noreply.github.com>
2020-07-01 20:38:25 -0700

    updating commands to load/fetch example db

--- etc/db-example/csv-to-postgres.sh
+++ etc/db-example/csv-to-postgres.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for file in "$SCRIPT_DIR"/*.csv; do
-  sudo docker cp "$file" postgres_db:/tmp/;
+  sudo docker cp "$file" postgresql-1:/tmp/;
 done;
 
-echo "Using postgress username='user' for this example."
-echo "The default password for 'user' is 123456789"
-psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c "COPY app_doctor FROM '/tmp/app_doctor.csv' DELIMITER ',' CSV HEADER;"
-psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c "COPY app_patient FROM '/tmp/app_patient.csv' DELIMITER ',' CSV HEADER;"
-psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c "COPY app_test FROM '/tmp/app_test.csv' DELIMITER ',' CSV HEADER;"
-psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c "COPY app_trackingchart FROM '/tmp/app_trackingchart.csv' DELIMITER ',' CSV HEADER;"
+echo "Using postgress username='user' password='123456789' for this example."
+sudo docker exec -it postgresql-1 bash -c "export PGPASSWORD='123456789'; psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c \"\copy app_doctor FROM '/tmp/app_doctor.csv' DELIMITER ',' CSV HEADER;\""
+sudo docker exec -it postgresql-1 bash -c "export PGPASSWORD='123456789'; psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c \"\copy app_patient FROM '/tmp/app_patient.csv' DELIMITER ',' CSV HEADER;\""
+sudo docker exec -it postgresql-1 bash -c "export PGPASSWORD='123456789'; psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c \"\copy app_test FROM '/tmp/app_test.csv' DELIMITER ',' CSV HEADER;\""
+sudo docker exec -it postgresql-1 bash -c "export PGPASSWORD='123456789'; psql -h 172.0.0.10 -p 5432 -U user -d doctorCRM -c \"\copy app_trackingchart FROM '/tmp/app_trackingchart.csv' DELIMITER ',' CSV HEADER;\""
+
